de7e6ce4ed99745ef2db84f52bb08a2bfcbb76f5
Suraj singh <atsurajsingh@gmail.com>
2024-09-03 13:35:10 +0530

    grid setup

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -3,6 +3,7 @@ package testBase;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
@@ -12,11 +13,14 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -35,15 +39,51 @@ public Logger logger;
 		p.load(file);
 	logger=	LogManager.getLogger(this.getClass());
 	
-		switch(browser.toLowerCase()){
+	
+	if(p.getProperty("execution_env").equalsIgnoreCase("remote"))
+	{
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		
+	//	capabilities.setPlatform(Platform.WIN11);
+	//	capabilities.setBrowserName("chrome");
+		
+		
+		// OS
+		if(os.equalsIgnoreCase("windows")) {
+			
+			capabilities.setPlatform(Platform.WIN11);
+			}
+		else if(os.equalsIgnoreCase("mac")) {
+			capabilities.setPlatform(Platform.MAC);
+		}
+		else {
+			System.out.println("No Operating System");
+		return;
+		}
+		
+		// Browsers
+		
+		switch(browser.toLowerCase())
+		{
+		case "chrome": capabilities.setBrowserName("chrome");break;
+		case "firefox": capabilities.setBrowserName("firefox");break;
+		case "edge" : capabilities.setBrowserName("MicrosoftEdge");break;
+	     default : System.out.println("invalid browser");return;
+			}
+		
+		driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),capabilities);
+	}
+	
+	if(p.getProperty("execution_env").equalsIgnoreCase("local")){
+       switch(browser.toLowerCase()){
 		case "chrome": driver = new ChromeDriver(); break;
 		case "firefox": driver = new FirefoxDriver(); break;
 		case "edge": driver = new EdgeDriver(); break;
 		case "safari": driver = new SafariDriver(); break;
 		default : System.out.println("please enter istalled browser"); return;
 		}
-		
-		//driver = new ChromeDriver();
+	}
+	 //driver = new ChromeDriver();
 	driver.manage().deleteAllCookies();
 	driver.get(p.getProperty("URL"));
 	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
